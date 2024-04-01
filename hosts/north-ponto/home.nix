3099a5fe5b31a3ef4c872c32ccf6887cec3f4812
c-lonas <chaselonas@gmail.com>
2024-04-01 16:01:37 -0700

    add base16-themes file for theme changing updates soon

--- hosts/north-ponto/home.nix
+++ hosts/north-ponto/home.nix
@@ -36,28 +36,7 @@ in
     swaylock
     (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
     (import ../../scripts/wallsetter.nix { inherit pkgs; })
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
 
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-
-
-    # (pkgs.writeShellScriptBin "wallsetter" ''
-
-    #   WALLPAPER="/home/battery/shaded_landscape.png"
-    #   WALLPAPER2="/home/battery/nix-magenta.png"
-
-    #   ${pkgs.swww}/bin/swww img "$WALLPAPER2"
-    # '')
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
